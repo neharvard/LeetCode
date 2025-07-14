48e7324a7b94442ac52908b92514e2c03bda0d18
Umme Neha <64556598+neharvard@users.noreply.github.com>
2025-07-14 20:10:35 +0600

    Create 1290. Convert Binary Number in a Linked List to Integer.cpp

---
+++ Daily Challenge/Bit Manipulation/1290. Convert Binary Number in a Linked List to Integer.cpp
@@ -0,0 +1,21 @@
+/*
+    Leetcode Link               : https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer
+*/
+
+
+/************************************************************ C++ *****************************************************/
+//Approach-2 (Using bit manipulation to form the decimal number)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int ans = 0 ;
+        while(head){
+            ans = (ans<<1) | (head->val);
+            head = head->next;
+        }
+        
+        return ans;
+    }
+};
