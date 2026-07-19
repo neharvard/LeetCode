bb3f5598dcd9a039e50d82a17b7aeae29e518f5f
Umme Neha <64556598+neharvard@users.noreply.github.com>
2026-07-20 01:56:21 +0600

    Refactor intToRoman function implementation
    Refactored intToRoman function for clarity and efficiency.

--- LeetCode 150/12. Integer to Roman.cpp
+++ LeetCode 150/12. Integer to Roman.cpp
@@ -1,21 +1,59 @@
+/*
+    Company Tags                : Amazon, Facebook, Microsoft, Twitter, Apple, Google, Paypal, Bloomberg, Adobe, InfoEdge
+    Leetcode Link               : https://leetcode.com/problems/integer-to-roman/
+    GfG Link                    : https://practice.geeksforgeeks.org/problems/convert-to-roman-no/1
+    
+*/
+//Approach-1
 class Solution {
- public:
-  string intToRoman(int num) {
-    const vector<pair<int, string>> valueSymbols{
-        {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"},
-        {90, "XC"},  {50, "L"},   {40, "XL"}, {10, "X"},   {9, "IX"},
-        {5, "V"},    {4, "IV"},   {1, "I"}};
-    string ans;
+public:
+    string intToRoman(int num) {
+        vector<int>v = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        vector<string>symbol = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        
+        int sz = v.size();
+        string ans;
+        while(num){
+            int rem, times;
+            string s;
+            for(int i=0; i<sz; i++){
+                if(num >= v[i]){
+                    rem = num % v[i];
+                    times = num / v[i];
+                    s = symbol[i];
+                    break;
+                }
+            }
+            // cout<<"rem:"<<rem<<" t:"<<times<<" s: "<<s<<endl;
+            num = rem;
 
-    for (const auto& [value, symbol] : valueSymbols) {
-      if (num == 0)
-        break;
-      while (num >= value) {
-        num -= value;
-        ans += symbol;
-      }
+            for(int i=1; i<=times; i++) ans += s;
+        }
+
+        return ans;    
     }
+};
 
-    return ans;
-  }
+
+// More clean Code of Approach-1
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<int>v = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        vector<string>symbol = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        
+        int i=0;
+        string ans = "";
+     
+        while(num){
+            while(num >= v[i]){
+                num -= v[i];
+                ans += symbol[i];
+            }
+            i++;
+        }
+
+        return ans;    
+    }
 };
